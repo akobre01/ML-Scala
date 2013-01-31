1f4317dfc8bef38552088dfb69c7a47506982850
akobre01 <ari.kobren@gmail.com>
2013-01-30 19:15:31 -0500

    minor stylistic changes to perceptron implementation

--- src/main/scala/Perceptron.scala
+++ src/main/scala/Perceptron.scala
@@ -1,22 +1,28 @@
 import scala.util.Random
-class Perceptron(var _weights:Array[Double], val alpha: Double = 0.01) {
+class Perceptron(var _weights: Seq[Double], val alpha: Double = 0.01) {
 
   def weights = _weights
 
-  /* classify an instance (an instance is a list of Doubles) */
-  def classify(instance: Array[Double]): Int = {
-    val score = weights.zip(instance).map({case (x,y) => x * y}).sum
+  /* classify an instance (an instance is a Seq of Doubles) */
+  def classify(instance: Seq[Double]): Int = {
+    val score = weights
+		  .zip(instance)
+		  .map{ case (weight, featVal) => weight * featVal }
+		  .sum
     if (score > 0) 1 else 0
   }
 
   /* PRECONDITION:
-   *   Each instance must be the same size (and the size of w)
+   *   Each instance must be the same size (and the size of weights)
    */
-  def learn(instances: List[(Array[Double], Int)]): Unit = {
+  def learn(instances: Seq[(Array[Double], Int)]): Unit = {
 
-    def update_weights(instance:Array[Double], label:Int): Unit = {
-      _weights.zipWithIndex.map({ case (w,i) =>
-		      w + alpha * (label - classify(instance)) * instance(i)})
+    def update_weights(instance: Seq[Double], label:Int): Unit = {
+      _weights = _weights
+		    .zipWithIndex
+		    .map{ case (w, i) =>
+			w + alpha * (label - classify(instance)) * instance(i)
+		    }
     }
 
     for ((instance, label) <- instances) {
